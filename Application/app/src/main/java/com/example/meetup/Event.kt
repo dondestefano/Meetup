41ce06045ae83fe8849e4249c44a2eed82e7f93c
dondestefano <dondestefano@users.noreply.github.com>
2020-04-19 13:39:37 +0200

    First commit
    Project started.

---
+++ Application/app/src/main/java/com/example/meetup/Event.kt
@@ -0,0 +1,11 @@
+package com.example.meetup
+import android.icu.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.Month
+import java.util.*
+
+class Event (var name: String, var date: SimpleDateFormat)  {
+
+}
